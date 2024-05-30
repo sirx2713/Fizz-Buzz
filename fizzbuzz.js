4812b73cbdc277a9ac560be385d05a7c9f65cb30
Mabende <belindamabende@gmail.com>
2024-05-30 21:21:29 +0200

    Code in scripts!

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -3,8 +3,12 @@ let count = 1;
 
 function fizzbuzz() {
 
-    if (count % 3 === 0 || count % 5 === 0){
+    if (count % 3 === 0){
         output.push("fizz");
+    }
+
+    else if (count % 5 === 0){
+        output.push("buzz");
     }
     else {
         output.push(count);
